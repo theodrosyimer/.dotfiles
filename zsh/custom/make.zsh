71336af09838de99bc17e8d866a613b4a9843aec
Theodros Yimer <theox01@hotmail.fr>
2023-01-27 03:33:09 +0100

    fix(make.zsh): fix typo

--- zsh/custom/make.zsh
+++ zsh/custom/make.zsh
@@ -87,7 +87,7 @@ function mkzfc() {
   output_path="${2:-"${my_functions_dir:-"$(pwd)"}"}"
   editor=code
   editor_args=-gn
-  filename="$(echo ${1:l} | sed -e 's/^ */-/g' -e 's/ *$//g' -e 's/_/-/g' -e 's/ /-/g')"
+  filename="$(echo ${1:l} | sed -e 's/^ *//g' -e 's/ *$//g' -e 's/_/-/g' -e 's/ /-/g')"
 
   funcname="$(echo $filename | sed s/-/_/g)"
   content=$(pbpaste)
