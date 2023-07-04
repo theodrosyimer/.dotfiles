1af225bba54b8e0d0c4809db94ed7adea888c1fd
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2023-07-04 18:40:45 +0200

    feat(github.zsh): add `git push -u origin main`

--- zsh/custom/github.zsh
+++ zsh/custom/github.zsh
@@ -1,10 +1,11 @@
 # TODO add option to create a public repository
 function gh_repo_create_from_cwd() {
   local repo_visibility="${1:-private}"
-  gh repo create --source="$(pwd)" --remote=upstream "--$repo_visibility" --description=$2
+  gh repo create --source="$(pwd)" --remote=upstream "--$repo_visibility" --description=$2 && git push -u origin main
 }
 
 alias ghrls="gh repo list"
 alias ghrlpr="gh repo list --limit 100 --visibility=private"
 alias ghrlpu="gh repo list --limit 100 --visibility=public"
 alias ghrd="gh repo delete"
+alias ghcreate=gh_repo_create_from_cwd
