fcc5465966e4eb5c65a9ef9c5bea9e22794d2e80
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2023-11-13 17:39:40 +0100

    fix: fix script

--- zsh/custom/rename-video.zsh
+++ zsh/custom/rename-video.zsh
@@ -1,17 +1,25 @@
-function rename_video() {
-  local videos="$(find . -type f -iname "*.mp4")"
+function rename_videos_from_file() {
+  local videos="$(find . -type f -iname "*.mp4" | sort)"
   local videos_array=(${(@f)videos})
   local titles="$(cat $1)"
   local titles_array=(${(@f)titles})
 
   mkdir -p renamed-videos &&
     for ((i=1; i < ${#videos_array[@]} + 1; i++)); do
-        echo "Copied $videos_array[$i] to renamed-videos/$titles_array[$i].mp4"
+        local title="$(trim "$titles_array[$i]")"
+        local video="$(trim "$videos_array[$i]")"
+
+        printf "%s\n" "Copied $video to renamed-videos/$title.mp4"
+
+        if [[ -a "renamed-videos/$title.mp4" ]]; then
+          printf "%s\n" "renamed-videos/$title.mp4 already exist skipping..."
+          continue
+        fi
 
         if [[ $i -lt 10 ]]; then
-          cp "$videos_array[$i]" renamed-videos/"0$i-$titles_array[$i]".mp4
+          cp "$video" "renamed-videos/0$i-$title.mp4"
         else
-          cp "$videos_array[$i]" renamed-videos/"$i-$titles_array[$i]".mp4
+          cp "$video" "renamed-videos/$i-$title.mp4"
         fi
     done
 
