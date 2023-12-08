2f5e164cd6b0ce50f4ca554049565652b18ed799
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2023-12-08 01:00:52 +0100

    refactor: better use of my env

--- zsh/custom/fzf-searches.zsh
+++ zsh/custom/fzf-searches.zsh
@@ -13,7 +13,7 @@ function _dev() {
 #   "~/Design"
 #   )
 
-dir_list="$(find $DOTFILES ~/Code/projects ~/ ~/Code ~/Code/courses ~/Code/_templates ~/Code/_templates/dev ~/Code/_templates/dev/ts ~/Code/_templates/dev/js ~/Code/refs ~/Code/refs/js/sandbox ~/Design -mindepth 1 -maxdepth 1 -type d))"
+dir_list="$(find $DOTFILES $CODE $CODE_PROJECTS $CODE_REFS $CODE_TEMPLATES $CODE_TEMPLATES/dev $CODE_TEMPLATES/dev/ts $CODE_TEMPLATES/dev/js $JS_SANDBOX $CODE_COURSES $CODE_WORK  $HOME/Design $HOME -mindepth 1 -maxdepth 1 -type d)"
 
 fm "${dir_list}"
 }
