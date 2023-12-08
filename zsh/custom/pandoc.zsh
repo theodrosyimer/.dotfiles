5aac29f8964851d9d425390a77825c06087333d4
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2023-12-08 18:23:49 +0100

    Fix file_path variable in convert_html_md_from_cb function

--- zsh/custom/pandoc.zsh
+++ zsh/custom/pandoc.zsh
@@ -4,7 +4,16 @@ function convert_md_html_from_cb() {
   local file_path="$(slugify ${1:-myfile})"
   local title="${2:-"This is the title of the file"}"
 
-  echo -e "$(pbpaste)" | 'pandoc' -s --metadata title="$title" -f markdown_mmd -t html -o $file_path.html
+  echo -e "$(pbpaste)" | 'pandoc' -s --metadata title="$title" -f markdown_mmd -t html -o "$file_path.html"
+}
+
+# ! dependency to download: pandoc
+# ! dependency: slugify.zsh -> available in the repository
+function convert_html_md_from_cb() {
+  local file_path="$(slugify ${1:-myfile})"
+  local title="${2:-"This is the title of the file"}"
+
+  echo -e "$(pbpaste)" | 'pandoc' -s --metadata title="$title" -f html -t markdown_mmd -o "$file_path.md"
 }
 
 # ! dependency to download: pandoc
@@ -15,3 +24,7 @@ function convert_md_man_from_cb() {
 
   echo -e "$(pbpaste)" | 'pandoc' -f markdown_mmd -t man -o $file_path.1
 }
+
+alias mdtohtml=convert_md_html_from_cb
+alias htmltomd=convert_html_md_from_cb
+alias mdtoman=convert_md_man_from_cb
