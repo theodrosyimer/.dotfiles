2b599bb43c0957cce6b855b91b23f74fd006c7d0
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2023-12-08 12:01:57 +0100

    feat: add is_number function

--- zsh/custom/math.zsh
+++ zsh/custom/math.zsh
@@ -38,8 +38,24 @@ function is_nan() {
   # oneliner using functions above
   is_int $1 || is_float $1 && return 1
 
-  # Check for a number
-  if [[ $num =~ '^[+-]?[0-9]+$' ]] && [[ $num =~ '^[+-]?[0-9]*\.?[0-9]+$' ]] && return 1
+  # or check for a number and float number
+  if [[ $num =~ '^[+-]?[0-9]+$' ]] || [[ $num =~ '^[+-]?[0-9]*\.?[0-9]+$' ]] && return 1
+}
+
+function is_number() {
+
+  # local num
+  # # User Input...
+  # vared -p "Enter the number : " -c num
+
+  # or...
+  local num=$1
+
+  # oneliner using functions above
+  is_int $1 || is_float $1 && return 0
+
+  # or check for a number and float number
+  # if [[ $num =~ '^[+-]?[0-9]+$' ]] || [[ $num =~ '^[+-]?[0-9]*\.?[0-9]+$' ]] && return 0
 }
 
 # source: https://stackoverflow.com/a/13407864/9103915
