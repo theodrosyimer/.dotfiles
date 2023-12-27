ed61ffa62a59562b169bf30dda9078426f96a6e5
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2023-12-27 21:39:02 +0100

    refactor: refactor tags.zsh script to improve functionality

--- zsh/custom/tags.zsh
+++ zsh/custom/tags.zsh
@@ -1,21 +1,26 @@
 function tags_get_all_user_tags() {
+  local current_dir=$(pwd)
+
 	cd ~ &&
-		'tag' -tgf \* | 'rg' '^    ' | 'cut' -c5- | 'sort' -u | 'rg' -N --no-column '^(:1:    )(.+)' -r $1 '$2'
+		'tag' -tgf \* | 'rg' '^    ' | 'cut' -c5- | 'sort' -u | 'rg' -N --no-column '^(:1:    )(.+)' -r $1 '$2' && cd "$current_dir"
 }
 
 function tags_write_to_file() {
 	local default_path="$HOME/.my_tags"
 	local file_path=${1:-${default_path}}
-	tags_get_all_user_tags >$file_path
-	la $file_path
+	tags_get_all_user_tags > "$file_path"
+	la "$file_path"
 }
 
 function tags_show() {
-	cat $TAG_LIST
+	less $TAGS
 }
 
 function tags_get_total_count() {
 	tags_show | wc -l
+}
+function tags_add_to_file() {
+  local tags_selection="$(tags_show | fzf --multi)"
 }
 
 alias tags=tags_show
