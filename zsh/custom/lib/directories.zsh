17b0534c386c03268361040f050a1ce6ed883e55
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-01-19 11:43:38 +0100

    fix: restore directory aliases

--- zsh/custom/lib/directories.zsh
+++ zsh/custom/lib/directories.zsh
@@ -3,10 +3,11 @@ setopt auto_pushd
 setopt pushd_ignore_dups
 setopt pushdminus
 
-alias -g ...='../..'
-alias -g ....='../../..'
-alias -g .....='../../../..'
-alias -g ......='../../../../..'
+alias -g ..='cd ../'
+alias -g ...=cd '../..'
+alias -g ....='cd ../../..'
+alias -g .....='cd ../../../..'
+alias -g ......='cd ../../../../..'
 
 alias -- -='cd -'
 alias 1='cd -1'
