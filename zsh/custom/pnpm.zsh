138035c269cd821a850ceb37ef856137b5e474d0
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-01-22 04:53:24 +0100

    refactor: refactor pinit function to init_package_json function

--- zsh/custom/pnpm.zsh
+++ zsh/custom/pnpm.zsh
@@ -1,3 +1,10 @@
-function pinit() {
-	pnpm init && npm pkg set name="${1:-"${PWD:t}"}" type='module' version='0.0.0'
+function init_package_json() {
+  local repo_name="${1:-"${PWD:t:l}"}"
+	(pnpm init || npm init ) 2>/dev/null && npm pkg set \
+  name=$repo_name \
+  type='module' \
+  homepage="https://github.com/theodrosyimer/$repo_name#README" \
+  bug="https://github.com/theodrosyimer/$repo_name/issues" \
+  repository.type="git" \
+  repository.url="git+https://github.com/theodrosyimer/$repo_name.git"
 }
