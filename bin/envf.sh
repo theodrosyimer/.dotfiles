b8d1cee7ad2e48dcd5ccd53a2e128b67384fc637
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-01-25 05:16:52 +0100

    Add script to select environment file using fzf feat: add envf.sh script
    This commit adds a new script, `envf.sh`, which allows the user to select an environment file using fzf. If no `.env` file is found, an error message is displayed. The selected environment file is then processed to remove a specific pattern. The script also includes an alternative method using `sed`.

--- bin/envf.sh
+++ bin/envf.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+ENV_FILES=(.env*)
+
+[[ ${#ENV_FILES[@]} -eq "0" ]] && echo "No .env file was found" && return 1
+
 ENV_FILE=.env
 ENV_FILE_CONTENT="$(grep '^[^#].*$' "$ENV_FILE")"
 ENV_CHOICE="$(echo "$ENV_FILE_CONTENT" | sort -n | fzf)"
-
-[[ -z "$ENV_CHOICE" ]] && exit 0
 
 PATTERN='*="'
 REPLACE_STR="" # not needed but better to be explicit...
@@ -12,10 +14,6 @@ REPLACE_STR="" # not needed but better to be explicit...
 RESULT=${ENV_CHOICE/${PATTERN}/${REPLACE_STR}}
 printf "%s" "${RESULT:0:(${#RESULT} - 1)}"
 
-### `zsh` version
-# RESULT=${ENV_CHOICE//${~PATTERN}/${REPLACE_STR}}
-# printf "%s"  ${RESULT:0:(${#RESULT} - 1)}
-
 ### Using `sed`
 # shellcheck disable=SC2001
-printf "%s" "$ENV_CHOICE" | sed -e "s/^.*=\"\(.*\)\"$/\1/"
+# printf "%s" "$ENV_CHOICE" | sed -e "s/^.*=\"\(.*\)\"$/\1/"
