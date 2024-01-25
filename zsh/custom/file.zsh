845248c1d3379b4bd3832992efe1940c95066f7b
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-01-25 05:18:19 +0100

    refactor: update get_dirs_list function and remove unused code

--- zsh/custom/file.zsh
+++ zsh/custom/file.zsh
@@ -1,80 +1,25 @@
-alias dirsls="get_dirs_list_cwd"
+alias dirsls="get_dirs_list"
 
 function getExtension() {
   echo "${1##*.}"
   echo "${1:e}"
 }
 
-# list all files and directories in current directory
-# filter by using a glob pattern as argument
-# i use this because ls 'separates filenames with newlines'
-# see: [ParsingLs - Greg's Wiki](https://mywiki.wooledge.org/ParsingLs)
-function list() {
-  local paths=()
-  for f in *; do
-    if [[ -a $f ]]; then
-      paths+="$(pwd)/$f";
-      # continue
-    fi
-  done
-
-  print -l $paths;
+### Usage example
+#
+#   myarray=(${(f)$(get_dirs_list $HOME)[@]})
+#
+# code above is too cryptic...
+# and we are making the work twice...
+# better to inline code below when and where needed...
+#   local paths=("${1:-"$PWD"}"/*)
+#
+# just replace the variables/arguments
+# keeping it here for reference
+function get_dirs_list() {
+  local paths=("${1:-"$PWD"}"/*)
+  printf "%s\n" ${#paths}
 }
-
-function get_dirs_list_cwd() {
-  local paths=""
-  local input="${1:-"$PWD"}"
-  local origin_cwd="$PWD"
-
-  if [[ -n $input ]]; then
-    echo $input
-    cd "$input"
-  fi
-
-  for f in *; do
-    if [[ -a $f ]]; then
-      paths+="$(pwd)/$f\n";
-      # continue
-    fi
-  done
-
-  if [[ -n $input ]]; then
-    echo $input
-    cd $origin_cwd
-  fi
-
-  printf "%b" $paths;
-}
-
-# function get_dirs_list_cwd() {
-#   local paths=""
-#   local input=""
-
-#   if [[ -n $1 ]]
-#   then
-#     input=$1/*
-#   fi
-#   echo $input
-
-#   for f in $input; do
-#     if [[ -a $f ]]; then
-#       paths+="$(pwd)/$f\n";
-#       # continue
-#     fi
-#   done
-
-#   printf "%b" $paths;
-# }
-
-# function listp(){
-#   local paths=()
-#   for f in ${@:-*}
-#     if [[ -a $f ]]; then
-#       parallel --shuf -j+0 paths+={1} ::: "$f";
-#     fi
-
-#   print -l $paths;
-# }
 
 function get_basename_no_ext() {
   local path="${1:-$PWD}"
