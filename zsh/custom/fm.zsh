641bdf37e341ac929b2fe0ed3b5dcb0ab86c9211
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-08-18 20:48:56 +0200

    refactor: update file manager keybindings
    Update the keybindings in the file manager script to improve navigation and functionality.

--- zsh/custom/fm.zsh
+++ zsh/custom/fm.zsh
@@ -11,14 +11,15 @@ function fm() {
     --bind "ctrl-e:execute("$EDITOR" {})+close" \
     --bind "ctrl-i:execute-silent(cp -Ri {} .)+close" \
     --bind "ctrl-r:reload(echo \"$dir_list\")" \
+    --bind "ctrl-r:+change-prompt()" \
     --bind "ctrl-c:reload(ls)" \
-    --bind "ctrl-r:+change-preview(exa --group-directories-first --tree --level=3 {} | head -50)" \
-    --bind "ctrl-r:+toggle-preview" \
-    --bind "ctrl-d:change-prompt(Dirs >)" \
+    --bind "ctrl-p:+change-preview(exa --group-directories-first --tree --level=3 {} | head -50)" \
+    --bind "ctrl-p:+toggle-preview" \
+    --bind "ctrl-d:change-prompt(Dirs > )" \
     --bind "ctrl-d:+reload(find {} -type d -mindepth 1 -maxdepth 1)" \
     --bind "ctrl-d:+change-preview(exa --group-directories-first --tree --level=3 {} | head -50)" \
     --bind "ctrl-d:+toggle-preview" \
-    --bind "ctrl-f:change-prompt(Files >)" \
+    --bind "ctrl-f:change-prompt(Files > )" \
     --bind "ctrl-f:+reload(find {} -type f -mindepth 1 -maxdepth 1)" \
     --bind "ctrl-f:+change-preview(bat --color=always --style=numbers {})" \
     --bind "ctrl-f:+toggle-preview" \
