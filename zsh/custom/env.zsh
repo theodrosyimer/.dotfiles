76d3c3d497c235244d8e3508480c58a5b831738f
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-08-23 13:39:40 +0200

    feat: add envx to create an .env.example from an existing .env file

--- zsh/custom/env.zsh
+++ zsh/custom/env.zsh
@@ -3,26 +3,51 @@ function envf() {
 
   [[ "${#ENV_FILES}" -eq "0" ]] && echo "No .env file was found!" && return 0
 
-  local ENV_FILE=.env.local
-  local ENV_FILE_CONTENT="$(grep '^[^#].*$' "$ENV_FILE")"
-  local ENV_CHOICE="$(echo "$ENV_FILE_CONTENT" | sort -n | fzf)"
+  local ENV_FILE="${1:-"${ENV_FILES[1]}"}"
+  local ENV_UNCOMMENTED_FILE_CONTENT="$(grep '^[^#].*$' "$ENV_FILE")"
 
-  local PATTERN='*="'
-  local REPLACE_STR="" # not needed but better to be explicit...
+  local ENV_CHOICE="$(echo "$ENV_UNCOMMENTED_FILE_CONTENT" | sort -n | fzf --no-preview)"
 
-  local RESULT=${ENV_CHOICE//${~PATTERN}/${REPLACE_STR}}
-  printf "%s"  ${RESULT:0:(${#RESULT} - 1)}
+  [[ -z $ENV_CHOICE ]] && return 0
+
+  # local PATTERN='*="'
+  # local REPLACE_STR="" # not needed but better to be explicit...
+
+  # local ENV_VALUE=${ENV_CHOICE//${~PATTERN}/${REPLACE_STR}}
+
+  # echo $ENV_VALUE
+  # printf "%s"  ${ENV_VALUE:1:(${#ENV_VALUE} - 1)}
 
   ### Using `sed`
   # shellcheck disable=SC2001
-  # printf "%s" "$ENV_CHOICE" | sed -e "s/^.*=\"\(.*\)\"$/\1/"
+  # local ENV_KEY="$(printf "%s" $ENV_CHOICE | sed -e 's/^\(.*=\).*$/\1/')"
+
+  # ! does not work
+  # local ENV_VALUE="$(printf "%s" $ENV_CHOICE | sed -e "s/^.*=[\",\']\?\(.*\)[\",\']\?$/\1/")"
+  local ENV_VALUE="$(printf "%s" $ENV_CHOICE | sed -e "s/^.*=\"\([^\"].*\)\"|.*=\'\([^\'].*\)\'|.*=\(.*\)$/\1\2\3/")"
+
+  printf "%s\n" "${ENV_VALUE}"
 }
 
 function envg() {
   # env is already an alias for:
   #   `env | sort -s`
   env | rg -i --no-line-number --no-column "$1"
-
-  # env | sort -s | rg -i --no-line-number --no-column "$1"
 }
 
+function envx() {
+  local cyan="${(%):-%F{cyan}"
+  local reset="${(%):-%f}"
+  local ENV_FILE
+
+  [[ -z "$1" ]] && ENV_FILE=(.env*) || ENV_FILE=("$1")
+
+  [[ "${#ENV_FILE[@]}" -eq "0" ]] && echo "No .env file was found!" && return 1
+
+  local ENV_FILE_CONTENT=$(< ${ENV_FILE[1]})
+  local ENV_KEYS="$(printf "%s" $ENV_FILE_CONTENT | sed -e 's/^\(.*=\).*$/\1/')"
+
+  printf "%s" $ENV_KEYS > .env.example
+  printf "\n%s\n\n" "$cyan""Copied to .env.example$reset"
+  cat .env.example
+}
