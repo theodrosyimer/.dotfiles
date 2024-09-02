c532b7495455af93d92069730f4e227bbe3d5255
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-09-02 18:40:12 +0200

    refactor: create watchlist from selected directory

--- zsh/custom/watchlist.zsh
+++ zsh/custom/watchlist.zsh
@@ -1,13 +1,15 @@
-function wla() {
-  local maxdepth=1
-  local path="$(get_paths_from_finder_selection)"
-  local playlist_name="${${path:t:l:gs/ /_/:gs/-//:gs/__/_/}%%.*}"
+alias wlc=create_watchlist_from_selected_dir
 
-  local videos_paths="$(find "$path" -type f -iname "*.mp4" -maxdepth $maxdepth)"
-  # local videos_paths="$(fd -e mp4 --base-directory "$path" -d1)"
+function create_watchlist_from_selected_dir() {
+  local path="${1:-"$(get_paths_from_finder_selection)"}"
+  local watchlist_name="${${path:t:l:gs/ /_/:gs/-//:gs/__/_/}%%.*}"
+  local watchlist_path="$VIDEOS/watchlist/$watchlist_name.m3u8"
 
-  echo "DIR_PATH:\n$path\n"
-  echo "PLAYLIST_NAME:\n$playlist_name\n"
-  echo "VIDEOS:\n$videos_paths"
-  echo "$videos_paths" > "$VIDEOS/watchlist/$playlist_name.m3u8"
+  if [ -f "${watchlist_path}" ]; then
+    printf '%s\n' 'Watchlist updated'
+    return 1
+  fi
+
+  printf '%s\n' "$path"/*.mp4 > "$watchlist_path"
+  printf '%s\n' "Watchlist created -> $_cyan$watchlist_name$_reset"
 }
