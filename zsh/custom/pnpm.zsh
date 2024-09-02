a79e7687cae971187d5a32e064d2ccff44c943c8
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-09-02 18:39:51 +0200

    refactor: add pinit alias to initialize package.json

--- zsh/custom/pnpm.zsh
+++ zsh/custom/pnpm.zsh
@@ -1,14 +1,30 @@
+alias pinit=init_package_json
+
 function init_package_json() {
+
+  if [[ -f "package.json" ]] ; then
+    printf '\n%s\n' "File already exists at $_cyan$PWD/package.json$_reset" && return 1
+  fi
+
   local repo_name="${1:-"${PWD:t:l}"}"
-	(pnpm init || npm init ) 2>/dev/null && npm pkg set \
-  name=$repo_name \
-  type='module' \
-  homepage="https://github.com/theodrosyimer/$repo_name#README" \
-  bug="https://github.com/theodrosyimer/$repo_name/issues" \
-  repository.type="git" \
-  repository.url="git+https://github.com/theodrosyimer/$repo_name.git"
 
-  # cp $CODE_TEMPLATES/dev-config/docs/README.md .
+	(pnpm init || npm init ) >/dev/null && local content="$(jq \
+  ".name = \"$repo_name\" | \
+  .type = \"module\" | \
+  .homepage = \"https://github.com/theodrosyimer/$repo_name#README\" |  \
+  .bug = \"https://github.com/theodrosyimer/$repo_name/issues\" | \
+  .repository.type = \"git\" | \
+  .repository.url = \"git+https://github.com/theodrosyimer/$repo_name.git\" | \
+  .files = [\"dist\"] | \
+  .packageManager = \"pnpm@\"$(pnpm -v)\"\"" package.json)"
 
-  printf "# $repo_name" > README.md
+  # cp $CODE_TEMPLATES/dev-config/docs/app/README.md .
+
+  # if [[ -s "README.md" ]]; then
+  #   printf '%s\n' "File \"README.md\" is not empty"
+  # fi
+
+  # printf "# $repo_name" > README.md
+  printf "$content" > package.json
+  (cat package.json || < package.json) >/dev/null
 }
