3725e38e353dc14f03f3b836a0b9ef5732fcff16
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-09-06 17:02:23 +0200

    refactor: update alias for getting Docker MySQL password

--- zsh/custom/docker.zsh
+++ zsh/custom/docker.zsh
@@ -2,4 +2,4 @@ function docker_mysql_get_password() {
 	docker container logs "${1:-"mysql"}" &> /dev/null | grep -o 'GENERATED ROOT PASSWORD.*$' | awk '{print $4}'
 }
 
-alias dkmysqlpwd='docker_mysql_get_password'
+alias dkmspwd='docker_mysql_get_password'
