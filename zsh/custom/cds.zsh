f37842fc8d62d8708848b64e817ac8469cb36cc9
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-10-14 11:20:19 +0200

    refactor: refactor cds function to use fcd and add file preview

--- zsh/custom/cds.zsh
+++ zsh/custom/cds.zsh
@@ -1,4 +1,4 @@
-# "cds ~/Documents" goes there and lists the files
-function cds() {
-  cd $1 && ls -la
+# "fcd ~/Documents" goes there and lists the files with a preview
+function fcd() {
+  cd "$(find ${1:-.} -type d -mindepth 1 -maxdepth 1 | sort | fzf --preview "exa --tree {}")";
 }
