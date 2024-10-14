3a7b7aaf998af6953110532d3c25b582e2a5e457
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-10-14 11:16:45 +0200

    refactor: update history command configuration in zsh
    - Disable displaying column and line numbers in ripgrep configuration
    - Use a variable for the path and improve sorting in the notes script
    - Add imgcat script for displaying images in iTerm2
    - Fix missing quotes in envs script
    - Add clear command to alternate-screen-buffer script
    - Configure zsh history settings:
      - Set HISTDUP to erase
      - Share command history data
      - Append to history file
      - Record timestamp of command in HISTFILE
      - Delete duplicates first when HISTFILE size exceeds HISTSIZE
      - Delete old recorded entry if new entry is a duplicate
      - Ignore duplicated commands history list
      - Ignore commands that start with space
      - Do not write duplicate entries in the history file
      - Do not display a line previously found
      - Show command with history expansion to user before running it

--- zsh/custom/lib/history.zsh
+++ zsh/custom/lib/history.zsh
@@ -31,10 +31,16 @@ esac
 [ "$HISTSIZE" -lt 50000 ] && HISTSIZE=50000
 [ "$SAVEHIST" -lt 10000 ] && SAVEHIST=10000
 
+HISTDUP=erase
+
 ## History command configuration
+setopt share_history          # share command history data
+setopt appendhistory          # append to history file
 setopt extended_history       # record timestamp of command in HISTFILE
 setopt hist_expire_dups_first # delete duplicates first when HISTFILE size exceeds HISTSIZE
+setopt hist_ignore_all_dups   # delete old recorded entry if new entry is a duplicate
 setopt hist_ignore_dups       # ignore duplicated commands history list
 setopt hist_ignore_space      # ignore commands that start with space
+setopt hist_save_no_dups      # do not write duplicate entries in the history file
+setopt hist_find_no_dups      # do not display a line previously found
 setopt hist_verify            # show command with history expansion to user before running it
-setopt share_history          # share command history data
