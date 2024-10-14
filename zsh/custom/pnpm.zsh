40e75dac4048a8b53b7133248d91ac049ff864f6
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-10-14 12:01:49 +0200

    refactor: update init_package_json function to copy README.md to specified directory

--- zsh/custom/pnpm.zsh
+++ zsh/custom/pnpm.zsh
@@ -1,30 +1,26 @@
 alias pinit=init_package_json
 
-function init_package_json() {
+function copyReadMeTo() {
+  cp $CODE_TEMPLATES/dev-config/docs/app/README.md "${1:-.}"
+}
 
-  if [[ -f "package.json" ]] ; then
-    printf '\n%s\n' "File already exists at $_cyan$PWD/package.json$_reset" && return 1
+function init_package_json() {
+  if [[ -f "package.json" ]]; then
+      printf '\n%s\n' "File already exists at $_cyan$PWD/package.json$_reset" && exit 1
   fi
 
   local repo_name="${1:-"${PWD:t:l}"}"
 
-	(pnpm init || npm init ) >/dev/null && local content="$(jq \
+  (pnpm init || npm init ) >/dev/null && \
+  cat package.json | jq \
   ".name = \"$repo_name\" | \
   .type = \"module\" | \
-  .homepage = \"https://github.com/theodrosyimer/$repo_name#README\" |  \
+  .homepage = \"https://github.com/theodrosyimer/$repo_name#README\" | \
   .bug = \"https://github.com/theodrosyimer/$repo_name/issues\" | \
   .repository.type = \"git\" | \
   .repository.url = \"git+https://github.com/theodrosyimer/$repo_name.git\" | \
   .files = [\"dist\"] | \
-  .packageManager = \"pnpm@\"$(pnpm -v)\"\"" package.json)"
+  .packageManager = \"pnpm@$(pnpm -v)\"" > package.json
 
-  # cp $CODE_TEMPLATES/dev-config/docs/app/README.md .
-
-  # if [[ -s "README.md" ]]; then
-  #   printf '%s\n' "File \"README.md\" is not empty"
-  # fi
-
-  # printf "# $repo_name" > README.md
-  printf "$content" > package.json
-  (cat package.json || < package.json) >/dev/null
+  cat package.json
 }
