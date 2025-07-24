229b07b9382cec009683a99535d70edbf58770c1
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2025-07-24 14:39:10 +0200

    fix: enhance get_process and extract_process_id functions for better filtering
    - Added alias `psg` for easier process retrieval.
    - Updated `get_process` to pass the PID filter correctly to `extract_process_id`.
    - Improved `extract_process_id` to handle PID filtering for piped input, files, and arguments.
    - Cleaned up commented-out debug statements for better readability.

--- zsh/custom/get-process-id.zsh
+++ zsh/custom/get-process-id.zsh
@@ -1,11 +1,14 @@
+alias psg="get_process"
+
 function get_process() {
-	ps aux | grep -i "${1}" | grep -v grep | extract_process_id
+	ps aux | grep -v grep | extract_process_id "$1"
 }
 
 function extract_process_id() {
   # Check to see if a pipe exists on stdin.
   if [ -p /dev/stdin ]; then
     # echo "Data was piped to this script!"
+    local pid_filter="$1"
 
     # If we want to read the input line by line
     # while IFS= read -r line; do
@@ -13,20 +16,35 @@ function extract_process_id() {
     # done
 
     # Or if we want to simply grab all the data, we can simply use redirection or `cat` instead
-    awk '{print $1" "$2" "$11}' < /dev/stdin
+    if [ -n "$pid_filter" ]; then
+      awk -v pid="$pid_filter" '$2 == pid {print $1" "$2" "$11}' < /dev/stdin
+    else
+      awk '{print $1" "$2" "$11}' < /dev/stdin
+    fi
 
   # Checking to ensure a filename was specified and that it exists
   elif [ -f "$1" ]; then
-      echo "Filename specified: $1"
-      echo "Doing things now.."
-      awk '{print $1" "$2" "$11}' < "$1"
+      # echo "Filename specified: $1"
+      # echo "Doing things now.."
+      local pid_filter="$2"
+      if [ -n "$pid_filter" ]; then
+        awk -v pid="$pid_filter" '$2 == pid {print $1" "$2" "$11}' < "$1"
+      else
+        awk '{print $1" "$2" "$11}' < "$1"
+      fi
 
   # Checking to ensure an argument was specified and that it exists
   elif [ -n "$1" ]; then
-      echo "Argument specified: $1"
-      echo "$1" | awk '{print $1" "$2" "$11}'
+      # echo "Argument specified: $1"
+      local pid_filter="$2"
+      if [ -n "$pid_filter" ]; then
+        echo "$1" | awk -v pid="$pid_filter" '$2 == pid {print $1" "$2" "$11}'
+      else
+        echo "$1" | awk '{print $1" "$2" "$11}'
+      fi
     else
-      echo "No input given!"
+      # echo "No input given!"
+      return 1
   fi
 }
 
