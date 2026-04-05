186a03e05bf0a36e7fac350e7de34f09bb39c581
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2026-04-05 04:59:28 +0200

    fix: fix tag scripts

--- zsh/custom/tags.zsh
+++ zsh/custom/tags.zsh
@@ -2,10 +2,11 @@ function tags_get_all_user_tags() {
   local current_dir=$(pwd)
 
   cd ~ &&
-  'tag' -tgf \* | 'rg' '^    ' | 'cut' -c5- | 'sort' -u | 'rg' -N --no-column '^(:1:    )(.+)' -r $1 '$2' && cd "$current_dir"
+  'tag' -tgf \* | 'rg' '^    ' | 'cut' -c5- | 'sort' -u && cd "$current_dir"
+  # 'tag' -tgf \* | 'rg' '^    ' | 'cut' -c5- | 'sort' -u | 'rg' -N --no-column '^[^\W](.+)' && cd "$current_dir"
 }
 
-function tags_write_to_file() {
+function tags_save_to_file() {
   local default_path="$HOME/.my_tags"
   local file_path=${1:-${default_path}}
   tags_get_all_user_tags > "$file_path"
@@ -14,20 +15,20 @@ function tags_write_to_file() {
 
 function tags_show() {
   if [ ! -f $TAGS ]; then
-    tags_write_to_file
+    tags_save_to_file
   fi
 
-  less $TAGS
+  cat "$TAGS" | fzf
 }
 
 function tags_get_total_count() {
-  tags_show | wc -l
+  cat "$TAGS" | wc -l
 }
 
 function tags_add_to_file() {
   local tags_selection="$(tags_show | fzf --multi)"
 }
 
-alias tags=tags_show
-alias tagsc=tags_get_total_count
-alias tagsw=tags_write_to_file
+alias tagshow=tags_show
+alias tagcount=tags_get_total_count
+alias tagsave=tags_save_to_file
